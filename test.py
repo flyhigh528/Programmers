9d12a5a2d13c5b3e2026104c6d620c42596563f1
flyhigh528 <purepis@naver.com>
2022-05-09 19:34:16 +0900

    2022.05.09 Programmers
    프로그래머스

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
-a = [1, 1, 3, 3, 0, 1, 1]
-b = set(a)
+li = ['Korea', 'America', 'China']  # li 초기화
+a=0                                                    # a 초기화
+str01 = ''                                           # str01 초기화
 
-print(b)
+for i in li:                                           # 바깥쪽 for반복문 수행
+   for j in i:                                         # 안쪽 for 반복문 수행
+      str01 += j[0]                              # j[0]을 str01에 추가
+      a = a + 1                                     # a를 1 증가 시킴
 
-#노트북 git 테스트
+      if a > 5:                                       # a가 5보다 크면
+         break                                       # 안쪽 for 반복문을 멈춤 
+
+print('a :', a, ', str01 :', str01)
